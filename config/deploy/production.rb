43ab931bb8660aed03d56cfdae7a331bec68180c
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-04-26 16:35:56 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
+set :rails_env, 'production'
 set :branch, 'master'
 
 role :app, %w{saiwaki@52.68.124.72}
